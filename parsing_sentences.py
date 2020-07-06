1af16732060b9000a30191f6d285cc8d752520c4
Aleksandra Konovalova <koalasaxen@gmail.com>
2020-07-06 12:30:24 +0300

    adding argument for removing proper nouns

--- parsing_sentences.py
+++ parsing_sentences.py
@@ -3,12 +3,13 @@ from collections import OrderedDict
 import os
 import pickle
 
-def parsing_sentences(df, name, lemma=False):
+def parsing_sentences(df, name, lemma=False, drop_propn=False):
 
     sentence = list(df["sentence"])
-    lemmas = list(df["lemma"]) #for now sentences are parsed only for original forms
+    lemmas = list(df["lemma"])
     original_forms = list(df["originalForm"])
     words_ids = list(df["wordOrderNumber"])
+    upos = list(df['upos'])
     
     if lemma:
         forms = lemmas
@@ -24,12 +25,24 @@ def parsing_sentences(df, name, lemma=False):
     curr = 0
     while curr < end:
         if words_ids[curr] == 1:
+            if drop_propn:
+                if upos[curr] == 'PROPN':
+                    curr += 1
+                    continue
             whole_sentence += forms[curr] + " "
             curr += 1
         else:
             try:
                 while words_ids[curr] != 1:
+                    if drop_propn:
+                        if upos[curr] == 'PROPN':
+                            curr += 1
+                            continue
                     whole_sentence += forms[curr] + " "
+                    if drop_propn:
+                        if upos[curr] == 'PROPN':
+                            curr += 1
+                            continue
                     curr += 1
             except IndexError:
                 whole_sentences.append(whole_sentence)
@@ -55,16 +68,20 @@ def parsing_sentences(df, name, lemma=False):
             continue
     try:
         df['sentenceText'] = all_sentences
-        with open("Z:/Documents/event extraction/all_sentences_lemmatized/{}".format(name), "w", encoding="utf-8") as r:
+        with open("Z:/Documents/event extraction/all_sentences/{}".format(name), "w", encoding="utf-8") as r:
             df.to_csv(r, index=False, header=True)
     except ValueError:
         df_err = pd.DataFrame({"Name": name}, index=[0])
-        with open("Z:/Documents/event extraction/errors.csv", "a", encoding="utf-8") as f:
+
+        ERRORS_FILE = "Z:/Documents/event extraction/errors.csv"
+
+        with open(ERRORS_FILE, "a", encoding="utf-8") as f:
             df_err.to_csv(f, header=False, index=False)
 
 if __name__ == "__main__":
-    directory = "Z:/Documents/event extraction/all_biographies/"
-    for filename in os.listdir(directory):
-        name = directory + filename
+    DIRECTORY = "Z:/Documents/event extraction/all_biographies/"
+
+    for filename in os.listdir(DIRECTORY):
+        name = DIRECTORY + filename
         df = pd.read_csv(name, encoding='utf-8', sep=';')
-        parsing_sentences(df, filename, lemma=True)
+        parsing_sentences(df, filename)
