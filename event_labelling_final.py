40c878df0c4b2c5e8a5917ba0347790c96fca894
Aleksandra Konovalova <koalasaxen@gmail.com>
2020-08-17 12:11:55 +0300

    changes in script

--- event_labelling_final.py
+++ event_labelling_final.py
@@ -17,19 +17,12 @@ def update_data(event, key_begin, key_end, labels):
     else:
         labels[sentences[row]].append(event)
 
-def remove_none(dicts):
-    clean_dict = dicts.copy()
-    for key in dicts.keys():
-        if dicts[key] == [None]:
-            del clean_dict[key]
-    return clean_dict
-
 errors = []
-directory = "Z:/Documents/event extraction/all_biographies/"
+directory = "Z:/Documents/event extraction/all_sentences/"
 print(directory)
 for filename in os.listdir(directory):
     name = directory + filename
-    df = pd.read_csv(name, encoding='utf-8', sep=';')
+    df = pd.read_csv(name, encoding='utf-8', sep=',')
     sent = []
     parag = []
     label = []
@@ -38,15 +31,17 @@ for filename in os.listdir(directory):
     #4 events with their most frequent structures and keywords stored in the dictionary 
     structures = {'avioliitto':[('solmia',{'avioliitto':'Case=Gen'}),('avioitua',),('mennä',{'naimisiin':''}),],
                   'kuolema':[('kuolla',),('hukkua',),('menehtyä',),('tehdä',{'itsemurha':'Case=Gen'}),],
-                  'muuttaminen':[('muuttaa','Case=Ill'),('lähteä','Case=Ill'),('siirtyä','Case=Ill'),],
+                  'muuttaminen':[('muuttaa','PROPN Case=Ill'),('lähteä','PROPN Case=Ill'),('siirtyä','PROPN Case=Ill'),],
                   'tutkinnon suorittaminen':[('suorittaa',{'tutkinto':'Case=Gen'}),('valmistua','Case=Tra'),('tulla',{'maisteri':'Case=Tra'}),('päästä',{'maisteri':'Case=Tra'}),],
                  }
     
-    #this function adds 'sentenceText' column to df, so removing it causes KeyError and events won't be labelled
-    parsing_sentences(df, filename, lemma=True)
+    #"parsing sentences" function adds 'sentenceText' column to df, so removing it causes KeyError and events won't be labelled
+    #if the directory contains files with "sentenceText", it can be omitted
+    #parsing_sentences(df, filename, lemma=True)
     
+    df['tags_and_features'] = df['upos'] + " " + df['feat']
     data = df.to_dict()
-
+    
     try:
         biographies = data['id'] #currently not used, but may be useful in the future
         lemmas = data['lemma']
@@ -54,10 +49,11 @@ for filename in os.listdir(directory):
         features = data['feat']
         sentences = data['sentence']
         sentences_text = data['sentenceText']
+        tags_and_features = data['tags_and_features']
 
         dicts = {'avioliitto':[], 'kuolema':[],'muuttaminen':[], 'tutkinnon suorittaminen':[]}
 
-        labels = dict(zip(sentences.values(), [[None]]*len(sentences)))
+        labels = dict(zip(sentences.values(), [["NO_LABEL"]]*len(sentences)))
 
         #iterating over all lemmas
         for key, value in lemmas.items():
@@ -80,9 +76,8 @@ for filename in os.listdir(directory):
 
                                         #finding the end of the structure
                                         #true if v is an empty string in case there are no particular morphological features
-                                        if lemmas[key_end] == k and v in str(features[key_end]):
+                                        if lemmas[key_end] == k and v in str(tags_and_features[key_end]):
                                             update_data(event, key_begin, key_end, labels)
-                                            print("yes")
                                             break
                                         else:
                                             key_end += 1
@@ -93,7 +88,7 @@ for filename in os.listdir(directory):
                                     print(j[1])
                                     try:
                                         while sentences[key_begin] == sentences[key_end]:
-                                            if j[1] in str(features[key_end]):
+                                            if j[1] in str(tags_and_features[key_end]):
                                                 update_data(event, key_begin, key_end, labels)
                                                 break
                                             else:
@@ -116,8 +111,9 @@ for filename in os.listdir(directory):
             df_results = pd.DataFrame({'Sentence': sent, "Sentence_Text": texts, 'Paragraph': parag, 'Label': label})
         except ValueError:
             errors.append(name)
-        with open("Z:/Documents/event extraction/all_reslts/{}".format(filename), "w", encoding="utf-8") as r:
+        with open("Z:/Documents/event extraction/test/{}".format(filename), "w", encoding="utf-8") as r:
             df_results.to_csv(r, index=False)
     except KeyError:
         errors.append(name)
+        print(name)
 pickle.dump(errors, open("Z:/Documents/event extraction/errors.pickle", "ab+"))
